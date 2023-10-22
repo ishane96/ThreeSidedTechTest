6d822f6630add339df1675f0d37ddd2b894ecb0a
ishane96 <achinthaishane96@gmail.com>
2023-10-22 22:33:18 +0100

    initial commit

---
+++ ThreeSidedTechTest/ThreeSidedTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeSidedTechTestApp.swift
+//  ThreeSidedTechTest
+//
+//  Created by Achintha kahawalage on 2023-10-22.
+//
+
+import SwiftUI
+
+@main
+struct ThreeSidedTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
